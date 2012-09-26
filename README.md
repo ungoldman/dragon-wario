e5d1d6c6618d841befb0dfa0cb2e8683410e8028
Nate Goldman <nnmgoldman@gmail.com>
2012-09-26 00:24:49 -0700

    add colors

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ node's built-in [readline](http://nodejs.org/api/readline.html) module.
 # How to Play
 
 * Make sure the latest version of [node.js](https://github.com/joyent/node/wiki/Installation) is installed.
-* Clone the repo, cd into the directory, type `node dw.js`. Enjoy!
+* Clone the repo, cd into the directory, then type:
 
-Note: This isn't a complete game, just a silly demo to show what node can do.
+```sh
+$ npm install
+$ node dw.js
+```
+
+* Enjoy!
+
+*Note*: This isn't a complete game, just a silly demo to show what node can do.
 
 # Author
 
--- dw.js
+++ dw.js
@@ -1,102 +1,130 @@
-var readline = require('readline'),
-    cl       = console.log,
-    thyHit   = 5,
-    slime    = 3,
-    name;
+var readline = require('readline')
+  , colors = require('colors')
+  , print = console.log
+  , thyHit = 5
+  , slime = 3
+  , experience = 0
+  , gold = 0
+  , name
+  , rl;
 
-var rl = readline.createInterface({
+rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-cl('\033[2J');
-cl("\nWelcome to Dragon Wario!\n");
+function pause(cb) {
+  rl.question('\n[ press enter to continue ]'.grey, function(ans){
+    clr();
+    cb();
+  });
+}
 
-rl.question('What is your name?\n> ',function(answer){
+clr();
+print('\nWelcome to '.white.bold+'Dragon Wario'.cyan.bold.underline+'!\n');
+
+rl.question('What is your name?\n\n'+'> '.green, function(answer){
   name = cleanStr(answer,true);
-  if (answer == '') name = 'derp';
+  if (answer == '') name = 'Derp';
   start();
 });
 
 function start() {
-  cl('\033[2J');
+  clr();
   if (name == 'Wario' || name == 'wario') {
-    cl("Yes, it's-a you, Wario!\n\n");
+    name = 'Wario'.rainbow.bold
+    print('Yes, it\'s-a you, '.bold+name+'!\n\n'.bold);
   }
-  cl('Ye Kinge: Greetings, '+name+'. Thou hast arrived in mine kingdom at a grave '+
-    'time indeed. The Slime Army of Westmere hath decimated mine trewps unto '+
-    'oblivione. If thou wouldst not protest too much methinks it goode if '+
-    'thou wouldst assist us with thy blade. Should you accept, thou shalt be '+
-    'most handsomely rewarded with garments and jewels, indeed.');
-  rl.question('\nDo you [a]ccept the Kinge\'s offer or [l]eave abruptly?\n> ',
-    function(ans){
-    ans = cleanStr(ans);
-    switch(ans){
-      case 'a':
-      case 'accept':
-        cl('\033[2J');
-        cl('Thou leavest ye Kinge\'s castle and traveleth unto yon field of battle.\n')
-        cl('\nA Slime draws near!');
-        fightLoop();
-        break;
-      case 'l':
-      case 'leave':
-        cl('\033[2J');
-        cl('Ye Kinge: Begone, foul knave!');
-        alternate();
-        break;
-      default: start();
-    }
-  })
+  name = name.green.bold;
+  print('Kinge\'s Castle.\n'.underline.bold);
+  print('Ye Kinge:'.magenta.bold+' \n\n'+
+    '"Greetings, '+name+'. \n\nThou hast arrived in mine kingdom at a grave time indeed. \n'+
+    'The '+'Slime'.red.bold+' Army of Westmere hath decimated mine trewps unto oblivione. \n'+
+    'If thou wouldst not protest too much methinks it goode if thou wouldst \n'+
+    'assist us with thy blade. Should you accept, thou shalt be most \n'+
+    'handsomely rewarded with garments and jewels, indeed."');
+  pause(function(){
+    rl.question('\n\nDo you '+
+      '['.red+'a'.green+']'.red+'ccept the '+'Kinge'.magenta.bold+'\'s offer or '+
+      '['.red+'l'.green+']'.red+'eave abruptly?\n\n'+'> '.green,
+      function(ans){
+      ans = cleanStr(ans);
+      switch(ans){
+        case 'a':
+        case 'accept':
+          clr();
+          print('Field of Battle.\n'.underline.bold);
+          print('Thou leavest ye '+'Kinge'.magenta.bold+'\'s castle and traveleth unto yon field of battle.');
+          print('A '+'Slime'.red.bold+' draws near!\n'.bold);
+          fightLoop();
+          break;
+        case 'l':
+        case 'leave':
+          clr();
+          print('Ye Kinge: \n'.magenta.bold+'"Begone, foul knave!"\n');
+          alternate();
+          break;
+        default:
+          print('\nChoose an real answer thou blaggard.'.red);
+          start();
+      }
+    });
+  });
 }
 
 function fightLoop() {
   if (thyHit < 1) {
-    gameOver();
-    process.exit(0);
+    print('Thou hast been defeated...');
+    pause(gameOver);
   }
   else if (slime < 1) {
-    cl('\nThou hast done well in defeating the Slime.'+
-    '\nThy experience increases by 1.'+
-    '\nThy GOLD increases by 1.');
-    interlude();
+    print('\nThou hast done well in defeating the '+'Slime'.red.bold+'.'+
+    '\nThy experience increases by '+'1'.green+'.'+
+    '\nThy '+'GOLD'.yellow.bold+' increases by '+'1'.green+'.\n');
+    experience++;
+    gold++;
+    pause(interlude);
   } else {
-    cl('\n[f]ight, [s]tatus, [r]un');
-    rl.question('Command?\n> ', function(ans){
+    print('Do you '+
+      '['.red+'f'.green+']'.red+'ight the '+'Slime'.red.bold+', '+
+      'check your '+'['.red+'s'.green+']'.red+'tatus, '+
+      'or attempt to '+'['.red+'r'.green+']'.red+'un?');
+    rl.question('\n'+'> '.green, function(ans){
       ans = cleanStr(ans);
       switch(ans){
         case 'f':
         case 'fight':
-          cl('\033[2J');
-          cl('\n'+name+" attacks!\nThe slime's Hit Point have been reduced by 1.\n");
+          clr();
+          print('\n'+name+' attacks!\nThe '+'slime'.red.bold+'\'s Hit Point have been reduced by '+'1'.red+'.\n');
           slime--;
-          cl("The Slime attacks!\nThy Hit decreased by 1.");
+          print('The '+'Slime'.red.bold+' attacks!\nThy Hit decreased by '+'1'.red+'.');
           thyHit--;
-          fightLoop();
+          pause(fightLoop);
           break;
         case 's':
         case 'status':
-          cl('\033[2J');
-          cl('\nThou hast ' + thyHit + ' Hit.');
-          cl('The Slime hath ' + slime + ' Hit Point.');
-          fightLoop();
+          clr();
+          print('\nThou hast ' + String(thyHit).green + ' Hit.');
+          print('The '+'Slime'.red.bold+' hath ' + String(slime).green + ' Hit Point.');
+          pause(fightLoop);
           break;
         case 'i':
         case 'item':
-          cl('\033[2J');
-          cl('\nThat cannot be used in battle.');
-          fightLoop();
+          clr();
+          print('\nThat cannot be used in battle.');
+          pause(fightLoop);
           break;
         case 'r':
         case 'run':
-          cl('\033[2J');
-          cl('\n'+name+' started to run away.\nBut was blocked from the front.');
-          cl('The Slime attacks!\nThy Hit decreased by 2.');
+          clr();
+          print('\n'+name+' started to run away.\nBut was blocked from the front.');
+          print('The '+'Slime'.red.bold+' attacks!\nThy Hit decreased by '+'2'.red+'.');
           thyHit--;
-          fightLoop();
+          pause(fightLoop);
           break;
         default:
-          cl('\nChoose an real answer thou blaggard.');
+          clr();
+          print('\nChoose an real answer thou blaggard.\n'.red);
           fightLoop();
       }
     });
@@ -104,93 +132,108 @@ function fightLoop() {
 }
 
 function interlude() {
-  cl('\nAfter an arduous duel with the vile Slime, you returnst to yon '+
-    'castle. But woe! It is burnt. It is no more.');
-  rl.question('Do you [s]earch the rubble for signs of life '+
-    'or [f]lee to safety?\n> ', function(ans){
+  print('Kinge\'s Castle.\n'.underline.bold);
+  print('After an arduous duel with the vile '+'Slime'.red.bold+', you returnst to yon castle.')
+  print('But woe! It is '+'burnt'.red+'. It is no more.\n');
+  rl.question('Do you '+
+    '['.red+''+'s'.green+']'.red+'earch the rubble for signs of life or '+
+    '['.red+''+'f'.green+']'.red+'lee to safety?\n\n'+'> '.green, function(ans){
       ans = cleanStr(ans);
       switch(ans){
         case 's':
         case 'search':
-          cl('\033[2J');
+          clr();
           endgame();
           break;
         case 'f':
         case 'flee':
-          cl('\033[2J');
-          cl('\nThy cowardly attempt to flee is thwarted when a Minotaur '+
-            'appears. Thou spontaneously combust from fear.');
-          gameOver();
-          process.exit(0);
+          clr();
+          print('\nThy cowardly attempt to flee is thwarted when a Minotaur appears. \n'+
+          'Thou spontaneously combust from fear.');
+          pause(gameOver);
           break;
         default:
-          cl('\033[2J');
-          cl('\nChoose an real answer thou blaggard.');
+          clr();
+          print('\nChoose an real answer thou blaggard.'.red);
           interlude();
       }
   })
 }
 
 function endgame() {
-  cl('\nUnder an rock thou findst the Kinge. He is dead. You take his '+
-    'crown. Thou art now the Kinge.\n'+
+  print('\nUnder an rock thou findst the '+'Kinge'.magenta.bold+'. He is dead. You take his '+
+    'crown. Thou art now the '+'Kinge'.magenta.bold+'.\n'+
     'A princess draws near!\n');
-  rl.question('Dost thou [m]arry her or [p]anic?\n> ', function(ans){
+  rl.question('Dost thou [m]arry her or [p]anic?\n\n'+'> '.green, function(ans){
     ans = cleanStr(ans);
     if (ans == 'm' || ans == 'marry') {
-      cl('\033[2J');
-      cl('Suddenly, thou art married and the Kinge. A winner is '+name+'.\n\n');
-      cl('\n\nTHE END\n\n');
+      clr();
+      print('Suddenly, thou art married and the '+'Kinge'.magenta.bold+'.'+
+        'A winner is '.bold+name+'.\n'.bold);
+      print('THE END'.underline.bold+'\n\n');
       process.exit(0);
     } else if (ans == 'p' || ans == 'panic') {
-      cl('\033[2J');
-      cl('Thou hast escapt the peril of a loveless marriage. Several years later '+
-        'thou sellst the Kinge\'s crowne for some prime beachfront property.');
-      cl('\nThou art retired. Congratulations, '+name+'. Thou hast survived '+
-        '"The Most Dangerous Game" and lived to tell the tale.');
-      cl('\n\nTHE END\n\n');
-      process.exit(0);
+      clr();
+      print('Thou hast escapt the peril of a loveless marriage. Several years later '+
+        'thou sellst the '+'Kinge'.magenta.bold+'\'s crowne for some prime beachfront property.\n');
+      pause(function(){
+        print('Beachfront Property.\n'.underline.bold);
+        print('Thou art retired. Congratulations, '+name+'. Thou hast survived '+
+          '"The Most Dangerous Game"'.bold+' and lived to tell the tale.');
+        print('\n\n'+'THE END'.underline.bold+'\n\n');
+        process.exit(0);
+      });
     } else {
-      cl('\033[2J');
-      cl('Thou hast failed to act in time. An errant meteor strikes you in '+
+      clr();
+      print('Thou hast failed to act in time. An errant meteor strikes you in '+
         'the phlegmatic humour.');
-      gameOver();
-      process.exit(0);
+      pause(gameOver);
     }
   })
 }
 
 function alternate() {
-  cl('\nYou travel the land in search of a worthy quest, but alas, most of '+
-    'them are boring and you are not qualified enough for the rest. After '+
-    'talking about it with your parents you decide to get an associate\'s '+
-    'degree in accounting. Two years pass by and you find yourself in the '+
-    'employ of a vast banking conglomerate. Several more years pass by and '+
-    'you find yourself in middle management. You are sent out to Westmere on '+
-    'a company retreat to increase synergy in the company.');
-  cl('\nAs you exit the terminal and attempt to hail a taxi, '+
-    'a Slime draws near!');
+  print('The Land.\n'.underline.bold);
+  print('You travel the land in search of a worthy quest, but alas, most of \n'+
+    'them are boring and you are not qualified enough for the rest. After \n'+
+    'talking about it with your parents you decide to get an associate\'s \n'+
+    'degree in accounting. After two years of studying you find yourself in \n'+
+    'the employ of a vast banking conglomerate. Several more meaningless \n'+
+    'years pass by imperceptibly and you find yourself in middle management. \n\n')
+  print('Conglomo Headquarters.\n'.underline.bold);
+  print('You are sent out to Westmere on a company retreat \n'+
+    'to increase '+'synergy'.yellow.bold+' in the company.\n\n');
+  print('Westmere International Airport.\n'.underline.bold);
+  print('As you exit the terminal and attempt to hail a taxi, \n'+
+    'a '+'Slime'.red.bold+' draws near!');
   fightLoop();
 }
 
 function gameOver() {
-  cl('\n     Thou art dead.');
-  cl(""+
-    "        _______\n"+
-    "     .-\"       \"-.\n"+
-    "    /             \\\n"+
-    "   /               \\\n"+
-    "   |   .--. .--.   |\n"+
-    "   | )/   | |   \\( |\n"+
-    "   |/ \\__/   \\__/ \\|\n"+
-    "   /      /^\      \\\n"+
-    "   \\__    '='    __/\n"+
-    "     |\\         /|\n"+
-    "     |\\'\"VUUUV\"'/|\n"+
-    "     \\ `\"\"\"\"\"\"\"` /\n"+
-    "      `-._____.-'");
-  cl('\n       GAME OVER\n\n');
+  print('\n     '+'Thou art dead.'.underline.bold);
+  print(""+
+    "        _______\n".red+
+    "     .-\"       \"-.\n".red+
+    "    /             \\\n".red+
+    "   /               \\\n".red+
+    "   |   .--. .--.   |\n".red+
+    "   | )/   | |   \\( |\n".red+
+    "   |/ \\__/   \\__/ \\|\n".red+
+    "   /      /^\      \\\n".red+
+    "   \\__    '='    __/\n".red+
+    "     |\\         /|\n".red+
+    "     |\\'\"VUUUV\"'/|\n".red+
+    "     \\ `\"\"\"\"\"\"\"` /\n".red+
+    "      `-._____.-'".red);
+  print('\n       GAME OVER\n\n'.red.bold);
+  process.exit(0);
 }
+
+function clr() {
+  print('\033[2J');
+}
+
+
 
 function cleanStr(str, upper){
   if (upper) return (str + '').trim();
